db445be8b1168c3794e87f9d510518f373f24da3
weijian <weijian.yang@thoughtworks.com>
2020-02-14 23:49:49 +0800

    add test to valid logic

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,9 +1,11 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
+
     public int calculate(int in) {
-        if (in == 3)
-            return 2;
-        return 1;
+        if (in == 1 || in == 2)
+            return 1;
+
+        return calculate(in - 1) + calculate(in - 2);
     }
 }
