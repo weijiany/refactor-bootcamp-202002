419ae11aaa811ae4698ad28a98c0528905a6a2a9
weijian <weijian.yang@thoughtworks.com>
2020-02-15 00:13:25 +0800

    use assertThat instead of assertEquals

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -1,10 +1,11 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FibonacciTest {
@@ -28,7 +29,7 @@ class FibonacciTest {
     @ParameterizedTest
     @MethodSource("generateValue")
     public void should_return_correct_value_when_calculate_given_specific_position(int in, long expect) {
-        assertEquals(expect, new Fibonacci().calculate(in));
+        assertThat(new Fibonacci().calculate(in), Matchers.equalTo(expect));
     }
 
     @Test
